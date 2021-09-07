aa25c2f9113c78d4310aff5da8ab0f7a7800395e
Reshma <reshma.satheesh2010@gmail.com>
2021-09-08 00:49:54 +0530

    First Commit: Implicit wait commented

---
+++ POMProject1/src/main/java/com/qa/constants/Constants.java
@@ -0,0 +1,13 @@
+package com.qa.constants;
+
+public class Constants {
+	
+public static final String logintitle="Login: Mercury Tours";
+
+public static final String Registerpagetitle="Register: Mercury Tours";
+
+public static final int timeout=60;
+
+
+
+}
---
+++ POMProject1/src/test/java/com/qa/testcases/BasePageTest.java
@@ -0,0 +1,38 @@
+package com.qa.testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.utility.WaitUtil;
+
+
+public class BasePageTest {
+	
+	 WebDriver driver; 
+	 
+	 
+	 @BeforeTest
+	 public void browserSetup() {
+		  	
+		 
+		 System.setProperty("webdriver.chrome.driver", "C:\\Program Softwares\\Selenium Drivers\\chromedriver_win32\\chromedriver.exe"); 		
+		   driver=new ChromeDriver();
+			driver.manage().window().maximize(); 	
+			driver.manage().timeouts().implicitlyWait( WaitUtil.IMPLICIT_WAIT,TimeUnit.SECONDS);
+//			 driver.get("https://demoqa.com/automation-practice-form");
+			 
+	 }
+
+	 
+	 @AfterTest 
+	 
+	 public void closeBrowser() {
+		 
+		 driver.close();
+	 }
+}
