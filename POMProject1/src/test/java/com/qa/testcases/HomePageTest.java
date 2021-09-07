97c052c41b1f6c725ab4c7899fc46bd351d74846
Reshma <reshma.satheesh2010@gmail.com>
2021-09-08 01:01:23 +0530

    Added Home PageTest

---
+++ POMProject1/src/test/java/com/qa/testcases/HomePageTest.java
@@ -0,0 +1,19 @@
+package com.qa.testcases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class HomePageTest {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
